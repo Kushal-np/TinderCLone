98a961b25aaba73fab3cf5b725727a09b6e53e9b
Kushal-np <wrckushal@gmail.com>
2025-10-18 23:53:21 +0545

    Added the files

--- Backend/routes/profile.js
+++ Backend/routes/profile.js
@@ -1,17 +1,32 @@
 import express from "express "; 
+import { userAuth } from "../middlewares/auth";
+import { validateProfileData } from "../utils/validation";
 const router = express.Router();
 
 
-router.get("/profile/view", async(req , res) =>{
-    try{
+router.get("/profile/view", userAuth,  async(req , res) =>{
+try{
         const user = req.user;
         res.send(user);
     }
     catch(error){
+        success:false , 
         res.status(401).json({
-            success:false , 
-            message:"Couldn't react upto the user" , 
+        message:"Couldn't react upto the user" , 
             error:error.message
         })
     }
 })
+//View logic
+router.patch("/profile/view" , userAuth , async(req , res)=>{
+    try{
+        if(!validateProfileData(req)){
+            throw new Error("Please enter valid input ")
+        }
+        const loggedInUser = req.user;
+
+    }
+    catch(error){
+
+    }
+} )
